fbec4ceab6297045bf073fbe10241f4ae82b6d71
luisromo <lromo@iteso.mx>
2020-06-09 20:17:19 -0500

    Third Commit 🥶

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,6 @@
 describe("First Testing lot", () => {
     let page;
     before(async () => {
-        // browser = await puppeteer.launch(
-        //     {
-        //         headless: false,
-        //         slowMo: 250,
-        //     }
-        // )
         page = await browser.newPage()
         await page.goto('https://google.com', { waitUntil: 'load' })
         page.setViewport({ width: 1920, height: 1080 })
